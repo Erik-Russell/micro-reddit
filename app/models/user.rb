8a4ce07f8b638c5f22f4340aa8e19b7fead1a4ca
Erik-Russell <erik.z.russell@gmail.com>
2024-11-06 08:13:09 -0800

    Proper Password Input
    password input on new user page will now create password-digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
   has_secure_password # enable bcrypt for password management
     # requires bcrypt in Gemfile
+
+  # Validations
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 10 }
 end
