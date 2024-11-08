0b04f9fd8cd4a7484f96001e5a3634cc7e886f32
Erik-Russell <erik.z.russell@gmail.com>
2024-11-08 12:12:08 -0800

    Fixed Typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destory
+  has_many :comments, dependent: :destroy
 
   has_secure_password
   # enable bcrypt for password management
