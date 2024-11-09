a0e65ca9052a5e2f0b61e65a67a0036c68dc01c7
Erik-Russell <erik.z.russell@gmail.com>
2024-11-08 18:06:22 -0800

    Post Validation
    Improved validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   # Validations
-  validates :title, length: { minimum: 2 }, presence: true
+  validates :title, length: { in: 2..50 }, presence: true
+  validates :body, presence: true, length: { in: 3..500, message: "must be at least 3 chars and less than 500 chars." }
 end
